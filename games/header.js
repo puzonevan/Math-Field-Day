fb9ee60f5d35043ecfdeddafa4f341099dab1073
‘Evan <puzonevan@gmail.com>
2021-10-19 21:30:25 -0700

    refactor header.js

--- games/header.js
+++ games/header.js
@@ -15,62 +15,69 @@ const closeLightbox = (name) => {
     document.getElementById("board").style.filter = "blur(0)";
     document.getElementById(`${name}-lightbox`).style.visibility = "hidden";
     document.getElementById(`${name}-lightbox`).style.opacity = "0";
+
+    switch(name){
+        case "rules": 
+            rulesStatus = false;
+            break;
+        case "new-game":
+            newGameStatus = false;
+            break;
+        case "menu": 
+            menuStatus = false;
+            break;
+    }
+
 };
 
 const openLightbox = (name) =>{
     document.getElementById("board").style.filter = "blur(1.5rem)";
     document.getElementById(`${name}-lightbox`).style.opacity = "1";
     document.getElementById(`${name}-lightbox`).style.visibility = "visible";
+
+    switch(name) {
+        case "rules": 
+            rulesStatus = true;
+            newGameStatus = false;
+            menuStatus = false;
+            document.getElementById("new-game-lightbox").style.visibility = "hidden";
+            document.getElementById("menu-lightbox").style.visibility = "hidden";
+            break;
+        case "new-game":
+            rulesStatus = false;
+            newGameStatus = true;
+            menuStatus = false;
+            document.getElementById("rules-lightbox").style.visibility = "hidden";
+            document.getElementById("menu-lightbox").style.visibility = "hidden";
+            break;
+        case "menu": 
+            rulesStatus = true;
+            newGameStatus = false;
+            menuStatus = true;
+            document.getElementById("new-game-lightbox").style.visibility = "hidden";
+        document.getElementById("rules-lightbox").style.visibility = "hidden";
+            break;
+    }
+
 };
 
 const rulesLightbox = rulesButton.addEventListener('click', (e) =>{
 
-    if(rulesStatus){
-        closeLightbox("rules");
-        rulesStatus = false;
-    }
-    else{
-        openLightbox("rules");
-        document.getElementById("new-game-lightbox").style.visibility = "hidden";
-        document.getElementById("menu-lightbox").style.visibility = "hidden";
-        rulesStatus = true;
-        newGameStatus = false;
-        menuStatus = false;
-    }
+    rulesStatus ? closeLightbox("rules") : openLightbox("rules");
 
 });
 
 
 const newGameLightbox = newGameButton.addEventListener("click", (e) =>{
     
-    if(newGameStatus){
-        closeLightbox("new-game");
-        newGameStatus = false;
-    }
-    else{
-        openLightbox("new-game");
-        document.getElementById("rules-lightbox").style.visibility = "hidden";
-        document.getElementById("menu-lightbox").style.visibility = "hidden";
-        newGameStatus = true;
-        rulesStatus = false;
-        menuStatus = false;
-    }
+    newGameStatus ? closeLightbox("new-game") : openLightbox("new-game");
 
 });
 
 
 const menuLightbox = menuButton.addEventListener("click", (e) =>{
-    if(menuStatus){
-        closeLightbox("menu");
-        menuStatus = false;
-    }
-    else{
-        openLightbox("menu");
-        document.getElementById("new-game-lightbox").style.visibility = "hidden";
-        document.getElementById("rules-lightbox").style.visibility = "hidden";
-        menuStatus = true;
-        newGameStatus = false;
-        rulesStatus = false;
-    }
+
+    menuStatus ? closeLightbox("menu") : openLightbox("menu");
+
 });
 
