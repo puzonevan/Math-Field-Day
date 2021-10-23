ca18125f09bc88376bf4f1ae9596667adc9333a0
‘Evan <puzonevan@gmail.com>
2021-10-23 00:29:40 -0700

    refactor header/rules.js

--- games/menu.js
+++ games/menu.js
@@ -11,77 +11,53 @@ const ticTacToeButton = document.getElementById("menu-tic-tac-toe");
 const hexButton = document.getElementById("menu-hex");
 const mancalaButton = document.getElementById("menu-mancala");
 
-// Five in a Row click 
-fiveInARowButton.addEventListener('click', (e) =>{
 
-    // Change the game name 
-    document.getElementById("game-title-current").innerHTML = "Five in A Row";
+const menuButton = (game) =>{
+    
+    // Change game name 
+    document.getElementById("game-title-current").innerHTML = game;
 
-    // Change the game rules
-    document.getElementById("game-name").innerHTML = "Five in a Row";
-    document.getElementById("game-rules").innerHTML = rules['five-in-a-row'];
+    // Change game rules 
+    document.getElementById("game-name").innerHTML = game;
+    document.getElementById("game-rules").innerHTML = rules[game];
 
     // Close the menu and open the rules 
     closeLightbox("menu");
     openLightbox("rules");
+
+}
+
+// Five in a Row click 
+fiveInARowButton.addEventListener('click', (e) =>{
+
+    menuButton("Five in a Row");
+
 });
 
 // Capture click 
 captureButton.addEventListener('click', (e) =>{
 
-    // Change the game name 
-    document.getElementById("game-title-current").innerHTML = "Capture";
-    
-    // Change the game rules
-    document.getElementById("game-name").innerHTML = "Capture";
-    document.getElementById("game-rules").innerHTML = rules.capture;
+    menuButton("Capture");
 
-    // Close the menu and open the rules 
-    closeLightbox("menu");
-    openLightbox("rules");
 });
 
 // 3D-tic-tac-toe click 
 ticTacToeButton.addEventListener('click', (e) =>{
 
-    // Change the game name 
-    document.getElementById("game-title-current").innerHTML = "3D-Tic-Tac-Toe";
+    menuButton("3D Tic Tac Toe");
 
-    // Change the game rules
-    document.getElementById("game-name").innerHTML = "3D-Tic-Tac-Toe";
-    document.getElementById("game-rules").innerHTML = rules['tic-tac-toe-3d'];
-
-    // Close the menu and open the rules 
-    closeLightbox("menu");
-    openLightbox("rules");
 });
 
 // Hex click 
 hexButton.addEventListener('click', (e) =>{
 
-    // Change the game name 
-    document.getElementById("game-title-current").innerHTML = "Hex";
+    menuButton("Hex");
 
-    // Change the game rules
-    document.getElementById("game-name").innerHTML = "Hex";
-    document.getElementById("game-rules").innerHTML = rules.hex;
-
-    // Close the menu and open the rules 
-    closeLightbox("menu");
-    openLightbox("rules");
 });
 
 // Mancala click 
 mancalaButton.addEventListener('click', (e) =>{
 
-    // Change the game name 
-    document.getElementById("game-title-current").innerHTML = "Mancala";
+    menuButton("Mancala");
 
-    // Change the game rules
-    document.getElementById("game-name").innerHTML = "Mancala";
-    document.getElementById("game-rules").innerHTML = rules.mancala;
-
-    // Close the menu and open the rules 
-    closeLightbox("menu");
-    openLightbox("rules");
 });
