7eb71f27424e1b8aa32d3e2e4ee229af97e372d0
‘Evan <puzonevan@gmail.com>
2021-10-24 18:02:32 -0700

    comments newGame.js :

--- games/newGame.js
+++ games/newGame.js
@@ -1,8 +1,32 @@
+/************* IMPORTS *************/
 import { closeLightbox } from './header.js';
+// Import Restart Board Function
 
+
+/////////////////////////////////////////////////////////////////////
+
+/************* BUTTON VARIABLES *************/
 const cancel = document.getElementById('cancel');
 const restart = document.getElementById('restart');
 
+/////////////////////////////////////////////////////////////////////
+
+/************* DOM EVENTS *************/
+
+// Cancel Button click 
 cancel.addEventListener('click', () =>{
+    
+    // Close the Lightbox
     closeLightbox('new-game');
+
+});
+
+// Restart Button click 
+restart.addEventListener('restart', () =>{
+    
+    // Close the Lightbox 
+    closeLightbox('new-game');
+
+    // Restart the game 
+
 });
