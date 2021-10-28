7d0803fc598d44bf4bf397bfba97dde810c3d2bc
‘Evan <puzonevan@gmail.com>
2021-10-27 20:50:22 -0700

    refactor into menu.js

--- games/menu.js
+++ games/menu.js
@@ -1,6 +1,9 @@
 /************* IMPORTS *************/
 import { closeLightbox, openLightbox } from './header.js';
+import { initializeBoard } from "./board/initialize.js";
+import { restartBoard } from "./board/restart.js";
 import {rules} from './rules.js';
+
 
 /////////////////////////////////////////////////////////////////////
 
@@ -39,28 +42,48 @@ const menuButton = (game) =>{
 fiveInARowButton.addEventListener('click', (e) =>{
     // Call Menu function
     menuButton("Five in a Row"); 
+
+    // Restart and Initialize the Board
+    restartBoard(document.getElementById("board"));
+    initializeBoard("five-in-a-row");
 });
 
 // Capture click 
 captureButton.addEventListener('click', (e) =>{
     // Call Menu function
     menuButton("Capture");
+
+    // Restart and Initialize the Board
+    restartBoard(document.getElementById("board"));
+    initializeBoard("capture");
 });
 
 // 3D-tic-tac-toe click 
 ticTacToeButton.addEventListener('click', (e) =>{
     // Call Menu function
     menuButton("3D Tic Tac Toe");
+
+    // Restart and Initialize the Board
+    restartBoard(document.getElementById("board"));
+    initializeBoard("tic-tac-toe-3d");
 });
 
 // Hex click 
 hexButton.addEventListener('click', (e) =>{
     // Call Menu function 
     menuButton("Hex");
+
+    // Restart and Initialize the Board
+    restartBoard(document.getElementById("board"));
+    initializeBoard("hex");
 });
 
 // Mancala click 
 mancalaButton.addEventListener('click', (e) =>{
     // Call Mancala function
     menuButton("Mancala");
+
+    // Restart and Initialize the Board
+    restartBoard(document.getElementById("board"));
+    initializeBoard("mancala");
 });
