74ec82208fbe94e588e5acba69370de97c9e3512
‘Evan <puzonevan@gmail.com>
2021-12-02 02:11:09 -0800

    player class export import

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -8,7 +8,10 @@ import { Mancala } from "./boards/mancala.js";
 import { TicTacToe3D } from "./boards/tic-tac-toe-3d.js";
 
 // Header
-
+import { menuLightbox } from "./header/menu.js";
+import { rulesLightbox } from "./header/rules.js";
+import { newGameLightbox } from "./header/new-game.js";
+import { Player } from "./header/player.js";
 
 /////////////////////////////////////////////////////////////////////
 
