45d6257fbbafe92b61caf93bec0163890b1f1c84
‘Evan <puzonevan@gmail.com>
2021-12-09 13:10:43 -0800

    basic moves capture class

--- source/assets/scripts/boards/capture.js
+++ source/assets/scripts/boards/capture.js
@@ -1,4 +1,5 @@
 /************* IMPORTS *************/
+import { Player } from "../header/player.js";
 import { createTable, createRow, createCol } from "./board.js"
 
 /////////////////////////////////////////////////////////////////////
@@ -6,9 +7,66 @@ import { createTable, createRow, createCol } from "./board.js"
 /************* CAPTURE CLASS *************/
 class Capture{
     
-    constructor(){
-
+    constructor(player1, player2){
+        this._player1 = player1; 
+        this._player2 = player2;
+        this._board = [[]];
+        this.initializeBoard();
+        this._flag = 0;
     }
+
+    /**
+     * initialize the behind the scenes board 
+     */
+    initializeBoard(){
+        let board = [];
+        for (let row = 0; row < 6; row++){
+            let row = [];
+            for(let col = 0; col < 6; col++){
+                row.push("");
+            }
+            board.push(row);
+        }
+        this._board = board;
+    }
+
+    /**
+     * start the game 
+     */
+    start(){
+        // Loop through each square
+        [...document.getElementById("td")].forEach((square, index) =>{
+
+            // Each square listens for a player move(click)
+            square.addEventListener("click", () =>{
+
+                // Do nothing if there is already an X or O
+                if(square.className === "y-player" || square.className === "x-player"){
+                    return;
+                }
+
+                // O Player's move 
+                if(this._flag === 0){
+
+                    // Display the Y player's icon
+                    square.setAttribute("class", "y-player");
+
+                    // X player's turn
+                    this._flag = 1;
+                }
+                else if(this._flag === 1){
+
+                    // Display the X Player's icon 
+                    square.setAttribute("class", "x-player");
+
+                    // Y Player's turn
+                    this._flag = 0;
+                }
+            });
+        });
+    }
+
+
 
     static createBoard(){
         // Capture Table
