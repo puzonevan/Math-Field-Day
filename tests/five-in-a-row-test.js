5a9051e7f8b4e1c78774bf43513ee3754ec07095
‘Evan <puzonevan@gmail.com>
2022-01-07 20:25:57 -0800

    five in a row basic test

---
+++ tests/five-in-a-row-test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+describe('Five in a Row', () =>{
+    describe('constructor', () => {
+        it('yes', ()=>{
+
+            // Setup 
+
+            // Exercise 
+            
+            // Verify 
+            assert.ok(4 === 4);
+        });
+    });
+    
+});
