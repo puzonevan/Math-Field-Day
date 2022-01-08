937ae72878b80c9a272181de4a5e63b47094b1b7
‘Evan <puzonevan@gmail.com>
2022-01-07 20:36:04 -0800

    five in a row setup constructor tests

--- tests/five-in-a-row-test.js
+++ tests/five-in-a-row-test.js
@@ -1,15 +1,26 @@
+import { FiveInARow } from '../source/assets/scripts/boards/five-in-a-row.js';
 const assert = require('assert');
 
-describe('Five in a Row', () =>{
-    describe('constructor', () => {
-        it('yes', ()=>{
+// Setup Five in a row class 
+const game = new FiveInARow("player1", "player2");
 
+// Five in a row class
+describe('Five in a Row', () =>{
+    // Constructor function
+    describe('.constructor', () => {
+        it('players', ()=>{
             // Setup 
 
             // Exercise 
-            
+
             // Verify 
             assert.ok(4 === 4);
+        });
+        it('initialize board', () =>{
+
+        });
+        it('flag', () =>{
+
         });
     });
     
