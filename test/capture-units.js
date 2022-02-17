c3e52dbac2c320802b9ab1c7a9341f013ecd0bd2
‘Evan <puzonevan@gmail.com>
2022-02-17 09:13:48 -0800

    capture test setup

---
+++ test/capture-units.js
@@ -0,0 +1,20 @@
+
+// Capture Function Unit Tests
+
+let initializeBoard = (board) =>{
+    for (let row = 0; row < 6; row++){
+        let row = [];
+        for(let col = 0; col < 6; col++){
+            row.push("");
+        }
+        board.push(row);
+    }
+}
+
+let isValidMove = (index) =>{
+
+}
+
+let isInRange = (index1, index2) =>{
+
+}
