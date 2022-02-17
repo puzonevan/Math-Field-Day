e0e4f7eb218a188c89380ff08ca284f6f7770aca
‘Evan <puzonevan@gmail.com>
2022-02-16 22:38:05 -0800

    testing

--- test/five-in-a-row.test.js
+++ test/five-in-a-row.test.js
@@ -2,3 +2,4 @@ const FiveInARow = require("../source/assets/scripts/boards/five-in-a-row");
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+// Basic Five in a Row Jest Test
