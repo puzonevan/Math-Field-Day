986c5dede72ada495094afe003f9fbd114335541
‘Evan <puzonevan@gmail.com>
2022-03-04 15:43:12 -0800

    five in a row corner case tests

--- admin/test/five-in-a-row.test.js
+++ admin/test/five-in-a-row.test.js
@@ -61,33 +61,97 @@ describe('7x7 board .checkWinner()', () => {
         test('bot left - all direction', () => {
             resetBoard();
             for(let i = 0; i < 5; i++){
-                board[0][i] = "X";
+                board[numRows - 1][i] = "X";
             }
-            let check = checkWinner("X", 0, 0, board, numRows, numCols);
+            let check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
             expect(check).toBeTruthy();
+            board[numRows - 1][2] = "O";
+            check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
+            expect(check).toBeFalsy();
 
             resetBoard();
             for(let i = 0; i < 5; i++){
-                board[i][0] = "X";
+                board[numRows - i - 1][0] = "X";
             }
-            check = checkWinner("X", 0, 0, board, numRows, numCols);
+            check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
             expect(check).toBeTruthy();
-            
+            board[numRows - 3][0] = "O";
+            check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
+            expect(check).toBeFalsy();
 
             resetBoard();
             for(let i = 0; i < 5; i++){
-                board[i][i] = "X";
+                board[numRows - i - 1][i] = "X";
             }
-            check = checkWinner("X", 0, 0, board, numRows, numCols);
+            check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
             expect(check).toBeTruthy();
+            board[numRows - 2][1] = "O";
+            check = checkWinner("X", numRows - 1, 0, board, numRows, numCols);
+            expect(check).toBeFalsy();
         });
 
-        test('bot right - all direction', () => {
+        test('top right - all direction', () => {
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[0][numCols - i - 1] = "X";
+            }
+            let check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[0][numCols - 2] = "O";
+            check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
 
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[i][numCols - 1] = "X";
+            }
+            check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[1][numCols - 1] = "O";
+            check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
+
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[i][numCols - i - 1] = "X";
+            }
+            check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[1][numCols - 2] = "O";
+            check = checkWinner("X", 0, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
         });
         
-        test('top right - all direction', () => {
+        test('bot right - all direction', () => {
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[numRows - 1][numCols - i - 1] = "X";
+            }
+            let check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[numRows - 1][numCols - 2] = "O";
+            check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
 
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[numRows - i - 1][numCols - 1] = "X";
+            }
+            check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[numRows - 2][numCols - 1] = "O";
+            check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
+
+            resetBoard();
+            for(let i = 0; i < 5; i++){
+                board[numRows - i - 1][numCols - i - 1] = "X";
+            }
+            check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeTruthy();
+            board[numRows - 2][numCols - 2] = "O";
+            check = checkWinner("X", numRows - 1, numCols - 1, board, numRows, numCols);
+            expect(check).toBeFalsy();
         });
     });
 });
