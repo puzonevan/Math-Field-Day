8d3838e31817bf692955ca1fff779d981a9ba7ee
‘Evan <puzonevan@gmail.com>
2022-03-04 14:25:12 -0800

    five in a row reset board helper

--- admin/test/five-in-a-row.test.js
+++ admin/test/five-in-a-row.test.js
@@ -16,8 +16,15 @@ const board = [
 const numRows = 7;
 const numCols = 7;
 
+const resetBoard = () => {
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[i].length; j++){
+            board[i][j] = "";
+        }
+    }
+}
 
 describe('7x7 board .checkWinner()', () => {
-    
+
 });
 // Basic Five in a Row Jest Test
