c77aae91d52df2141e151a1583db70e5518cbee7
‘Evan <puzonevan@gmail.com>
2022-03-06 20:50:21 -0800

    tic tac toe 3d setup describe and test methods

--- admin/test/tic-tac-toe-units.js
+++ admin/test/tic-tac-toe-units.js
@@ -1,3 +1,14 @@
+const winningConditions2D = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 6, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
 // 3-D Tic Tac Toe functions to unit test
 const checkWinner = (board, board3D, flag)=>{
 
--- admin/test/tic-tac-toe.test.js
+++ admin/test/tic-tac-toe.test.js
@@ -1,5 +1,27 @@
 const { checkWinner } = require('./tic-tac-toe-units.js');
 
 describe('.checkWinner()', () => {
+    describe('all possible combinations one board', () =>{
+        test('board 1', () => {
+
+        });
+        test('board 2', () => {
+
+        });
+        test('board 3', () => {
+
+        });
+    });
+
+    describe('all possible combinations across boards', () =>{
+        test('stacks', () => {
+
+        });
+        test('other', () =>{
+
+        });
+    });
+
     
+
 });
